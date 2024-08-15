70617b1b98cc099add126891176e329bcd0f0b10
Marcela Meiser <marcelameiserr@gmail.com>
2024-08-15 19:46:38 -0300

    Exercícios Linguagem C#

---
+++ curso_programacao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curso_programacao
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int idade = 20;
+            double salario = 1300.50;
+            double altura = 1.60;
+            char genero = 'F';
+            string nome = "Marcela Meiser";
+
+            Console.WriteLine(idade);
+            Console.WriteLine(salario);
+            Console.WriteLine(altura);
+            Console.WriteLine(genero);
+            Console.WriteLine(nome);
+        }
+    }
+}
